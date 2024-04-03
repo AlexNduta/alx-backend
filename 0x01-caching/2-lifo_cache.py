188da9010160a3dc5e25d9772b02e784bbca6e3d
was <was@localhost.localdomain>
2024-04-03 11:17:05 +0300

    working

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,19 +5,20 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 class LIFOCache(BaseCaching):
     def __init__(self):
-        self.cache_data = {}
         super().__init__()
 
     def put(self, key, item):
-        length = len(self.cache_data)
         if key and item:
+
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS:
+               # last_item = next(reversed(self.cache_data.keys()))
+                last_item = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(last_item)
+                print("DISCARD: {}".format(last_item))
+
             self.cache_data[key] = item
 
-        if length == BaseCaching.MAX_ITEMS:
-            last_item = next(reversed(self.cache_data.keys()))
-
-            self.cache_data.pop(last_item)
-            print("DISCARD:", last_item)
 
     def get(self, key):
         """gets a list from a dict"""
