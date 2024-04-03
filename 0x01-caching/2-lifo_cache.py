7b91128c651b43c8f0e7eda5e9bf78d377ab1e13
was <was@localhost.localdomain>
2024-04-03 10:54:46 +0300

    LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""module level documentation"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        self.cache_data = {}
+        super().__init__()
+
+    def put(self, key, item):
+        length = len(self.cache_data)
+        if key and item:
+            self.cache_data[key] = item
+
+        if length == BaseCaching.MAX_ITEMS:
+            last_item = list(self.cache_data.keys())[-1]
+
+            self.cache_data.pop(last_item)
+            print("DISCARD:", last_item)
+
+    def get(self, key):
+        """gets a list from a dict"""
+        if key:
+            return self.cache_data.get(key)
+        else:
+            return None
