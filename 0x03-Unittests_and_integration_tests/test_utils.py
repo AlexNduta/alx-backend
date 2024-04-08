dca84a63099989777b0cce8f2ae331c66a1cb7d2
was <was@localhost.localdomain>
2024-04-08 15:58:09 +0300

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,14 +6,14 @@ import Typing
 from unittest import unittest.TestCase
 nested_map = utils.access_nested_map
 
+
 @parameterized.expand([
     ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ({"a": {"b": 2}}, ("a",), {"b": 2}),
+    ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
 class TestAccessNestedMap(unittest.TestCase):
-    TestAccessNestedMap.test_access_nested_map(nested_map, path, expected):
+    """"test method"""
+    test_access_nested_map(self, nested_map, path, expected):
+        """Check if the required output is given"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-
-
