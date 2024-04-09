8beb29f6dfaa52a553a88c5998294aa43cf40ce1
was <was@localhost.localdomain>
2024-04-09 22:06:41 +0300

    project

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import typing
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route("/")
 
-
+@app.route("/")
 def welcome():
     """Serve the 0-index.html on the route / """
     return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
