d6cd95b7a0d46ea82139e0d1fa21185ab6b4a707
was <was@localhost.localdomain>
2024-04-13 04:36:20 +0300

    getting

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module level doc"""
+from flask import Flask, render_template
+from flask_babel import Babel
+setup = __import__('1-app').Config
+app = Flask(__name__)
+app.config.from_object(setup)
+babel = Babel(app)
+
+@babel.localselector
+def get_locale():
+    user = getattr(setup, 'user')
+    if user is not None:
+        return user.locale
+    return request.accept_languages(['en', 'fr'])
