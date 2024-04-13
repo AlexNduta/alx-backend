9523b6bdb6e5a3c70b135aaf3c351ff465260c3a
was <was@localhost.localdomain>
2024-04-13 04:58:51 +0300

    fixing

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,24 +3,30 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config(object):
+    """ class level doc """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localselector
 def get_locale():
     """Doc doc"""
     return request.accept_languages.best_match(app.Config.["LANGUAGES"])
 
+
 @app.route('/')
 def hello():
     """func doc hunc doc"""
     return render_template("2-index.html")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
